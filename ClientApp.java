9bfef54a7eeecd1eeaa5e617184119f0ff9cb4f1
arman18 <43715453+arman18@users.noreply.github.com>
2019-04-16 10:29:55 +0600

    Add files via upload

---
+++ ClientApp.java
@@ -0,0 +1,37 @@
+package hackathon;
+
+import java.net.*;  
+import java.io.*;  
+class MyClient{  
+public static void main(String args[])throws Exception{ 
+	System.out.println("Welcome to client. Write stop for close");
+	
+	Socket s=new Socket("localhost",3333);  
+	
+	DataInputStream din=new DataInputStream(s.getInputStream());  
+	DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
+	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
+	  
+	String str,str2;  
+	while(true){
+                try {
+                    str2=din.readUTF(); //after server calling dout.close()
+                                        //client attemp to using din.readUTF()
+                                        //will throws EOFException
+                }
+                catch (EOFException e) {
+                    break;
+                }
+                System.out.println(str2);  
+		str=br.readLine();  
+		dout.writeUTF(str);  
+		dout.flush();  
+	}  
+	  
+	dout.close();  
+	s.close();  
+	
+	System.out.println("Thank you.");
+
+	}
+}  
